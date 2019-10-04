a3633edde882a499afd6fa8a53ace13cc87b43e8
whxb69 <smvlboy@outlook.com>
2019-10-04 11:16:13 +0800

    功能基本完成 待优化

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ui import *
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import cgitb
@@ -12,20 +12,19 @@ cgitb.enable()
 class Newlable(QLineEdit):
     def __init__(self, parent=None):
         super().__init__(None, parent)
-        # self.poslog = []
         self.state = None
         self.setText('New Note')
         self.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
-                           "border-color: rgb(0, 0, 0);")
+                           "border-color: rgb(240,240,240);background-color:rgb(240,240,240)")
         self.setAcceptDrops(False)
         self.setReadOnly(True)
         self.setDisabled(False)
         self.mm = False  # move mode
         self.press = False
+        self.tgt = None
+        self.draw = False
 
     def mouseDoubleClickEvent(self, event):
-        # print('yes')
-        # self.setAcceptDrops(True)
         self.setReadOnly(False)
         self.setStyleSheet("border-width:2px;border-style: solid; "
                            "border-radius: 15px;border-color: rgb(150, 100, 0);")
@@ -33,32 +32,46 @@ class Newlable(QLineEdit):
     def mousePressEvent(self, QMouseEvent):
         self.press = True
 
-    def mouseMoveEvent(self, QMouseEvent):
-        # print(self.press)
-        # if self.state in ['in', 'select']:
+    def mouseMoveEvent(self, event):
         if self.press:
-            if not self.mm:
-                self.mm = True
-                self.focusNextChild()
-                self.label = templabel(self, self.x(), self.y(), win)
-                self.label.show()
-                self.label.setFocus()
-                print(self.label.hasFocus())
-            else:
-                x = self.label.x()
-                y = self.label.y()
-                self.label.move(x, y)
+            self.mm = True
+            if not hasattr(self, 'temp'):
+                self.temp = win.inittag(self.x()+90, self.y()+90)
+                self.temp.setObjectName(self.objectName())
+                self.setObjectName('temp')
+                self.setStyleSheet("border-style:none;background-color:rgb(240,240,240)")
 
-    def mouseReleaseEvent(self, QMouseEvent):
+            # if isinstance(temp,Newlable):
+            wx = win.x()
+            wy = win.y()
+            x = event.globalX()
+            y = event.globalY()
+            self.move(x - wx - 90, y - wy - 90)
+
+            alltag = win.findChildren(QLineEdit)
+            for tag in alltag:
+                tx = tag.x()
+                ty = tag.y()
+                distance = math.sqrt((self.x() - tx) ** 2 + (self.y() - ty) ** 2)
+                if distance <= 80.7:
+                    if tag not in [self.temp, self]:
+                        tag.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
+                                          "border-color: rgb(0, 0, 0);background-color: gray;")
+                        self.tgt = tag
+                else:
+                    tag.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
+                                      "border-color: rgb(0, 0, 0);")
+                    if self.tgt == tag:
+                        self.tgt = None
+
+    def mouseReleaseEvent(self, event):
         self.press = False
         if self.state == 'select':
             self.setReadOnly(False)
             self.setStyleSheet("border-width:2px;border-style: solid; "
                                "border-radius: 15px;border-color: rgb(150, 100, 0);")
             print(self.acceptDrops())
-            # label = templabel(self.text(), self.x(), self.y(), win)
-            # label.show()
-            # self.state = 'move'
+
         elif self.state == 'in':
             alltag = win.findChildren(QLineEdit)
             for tag in alltag:
@@ -71,123 +84,33 @@ class Newlable(QLineEdit):
             self.setStyleSheet("border-width:3px;border-style: solid; "
                                "border-radius: 15px;border-color: rgb(150, 100, 0);")
 
+        if self.mm:
+            self.draw = True
+            wx = win.x()
+            wy = win.y()
+            x = event.globalX()
+            y = event.globalY()
+            if self.tgt:
+                win.drawline_pt(self.temp, self.tgt)
+            else:
+                self.temp.move(x - wx - 90, y - wy - 90)
+                win.update()
+                self.tgt = None
+                self.mm = False
+            self.deleteLater()
+
     def enterEvent(self, event):
         if self.state != 'select':
             self.state = 'in'
             self.setStyleSheet("border-width:1.5px;border-style: dashed; "
-                               "border-radius: 15px;border-color: rgb(0, 0, 0);")
+                               "border-radius: 15px;border-color: rgb(0, 0, 0);"
+                               "background-color: (240,240,240)")
 
     def leaveEvent(self, event):
         if self.state != 'select':
             self.state = None
             self.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
-                               "border-color: rgb(0, 0, 0);")
-
-
-class templabel(QLabel):
-    def __init__(self, match, x, y, parent=None):
-        super().__init__(None, parent)
-        self.state = None
-        self.setText(match.text())
-        self.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
-                           "border-color: rgb(0, 0, 0);")
-        # self.setAcceptDrops(False)
-        self.setFocusPolicy(Qt.ClickFocus)
-        self.setGeometry(x, y, 150, 60)
-        self.match = match
-        self.mm = False
-        self.tgt = None
-        self.draw = False
-        self.press = True
-
-        # self.setFocus()
-
-    def getline(self, tagname):
-        tgt = win.findChild(QLineEdit, tagname)
-        if tgt != self.match:
-            tgt.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
-                              "border-color: rgb(0, 0, 0);background-color: gray;")
-            print(tgt)
-
-    def mouseMoveEvent(self, event):
-        if self.press:
-            if not self.mm:
-                self.mm = True
-            wx = win.x()
-            wy = win.y()
-            x = event.globalX()
-            y = event.globalY()
-            self.move(x - wx - 75, y - wy - 90)
-
-            alltag = win.findChildren(QLineEdit)
-            for tag in alltag:
-                tx = tag.x()
-                ty = tag.y()
-                distance = math.sqrt((self.x() - tx) ** 2 + (self.y() - ty) ** 2)
-                if distance <= 80.7:
-                    if tag != self.match:
-                        tag.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
-                                          "border-color: rgb(0, 0, 0);background-color: gray;")
-                        self.tgt = tag
-                else:
-                    tag.setStyleSheet("border-width: 0px;border-radius: 15px; border-style: solid;"
-                                      "border-color: rgb(0, 0, 0);")
-                    if self.tgt == tag:
-                        self.tgt = None
-
-    def mousePressEvent(self, QMouseEvent):
-        self.press = True
-
-    def mouseReleaseEvent(self, QMouseEvent):
-        self.match.mm = False
-        self.mm = False
-        wx = win.x()
-        wy = win.y()
-        x = QMouseEvent.globalX()
-        y = QMouseEvent.globalY()
-        print(self.tgt)
-        if self.tgt:
-            self.draw = True
-            # win.drawline(self.tgt.x() + 75, self.tgt.y() + 30,
-                         # self.match.x() + 75, self.match.y() + 30)
-            win.drawline_pt(self.match,self.tgt)
-        else:
-            self.match.move(x - wx - 75, y - wy - 90)
-        self.deleteLater()
-        win.update()
-        # self.l_fun.quit()
-
-
-# class Line(QThread):
-#     get = pyqtSignal(str)
-#
-#     def __init__(self,label, parent=None):
-#         super(Line,self).__init__(parent)
-#         self.label = label
-#         self.lx = label.x()
-#         self.ly = label.y()
-#
-#     def __del__(self):
-#         self.wait()
-#
-#     def run(self):
-#         while self.label.mm:
-#             # print(1)
-#             alltag = win.findChildren(QLineEdit)
-#             print(len(alltag))
-#             for tag in alltag:
-#                 tx = tag.x()
-#                 ty = tag.y()
-#                 distance = math.sqrt((self.lx-tx)**2+(self.ly-ty)**2)
-#                 # print(distance)
-#                 if distance <= 78.8:
-#                     print(self.lx,tx,self.ly,ty)
-#                     self.get.emit(tag.objectName())
-#                     #
-#                     # return tag
-
-# def stop(self):
-#     self.trigger.emit()
+                               "border-color: rgb(0, 0, 0);background-color: (240,240,240)")
 
 class Mainwindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -196,42 +119,39 @@ class Mainwindow(QMainWindow, Ui_MainWindow):
         # self.lines = []
         self.lines = {}
         self.draw = False
+        self.lpos = (None, None)
 
     def paintEvent(self, event):
-        # if self.draw:
-        if hasattr(self,'lines'):
+        if self.draw and hasattr(self, 'lines'):
             pen = QPainter(self)
             pen.begin(self)
             for s in self.lines:
                 for l in self.lines[s]:
-                    print(s,l)
                     ss = win.findChild(QLineEdit, s)
                     ee = win.findChild(QLineEdit, l)
-                    line = QLineF(ss.x()+75, ss.y()+30, ee.x()+75, ee.y()+30)
-                    line.setLength(line.length() - 70)
+                    line = QLineF(ss.x() + 90, ss.y() + 30, ee.x() + 90, ee.y() + 30)
+                    line.setLength(line.length())
                     pen.setPen(QPen(Qt.darkRed, 2, Qt.DashLine))
                     pen.drawLine(line)
                     self.drawarrow(pen, line)
-                # TODO:计算直线具体减少长度
             pen.end()
 
     def drawarrow(self, pen, line):
         v = line.unitVector()
         v.setLength(20)  # 改变单位向量的大小，实际就是改变箭头长度
-        v.translate(QPointF(line.dx(), line.dy()))
+        v.translate(QPointF(line.dx()/2, line.dy()/2))
+
         n = v.normalVector()  # 法向量
         n.setLength(n.length() * 0.5)  # 这里设定箭头的宽度
+
         n2 = n.normalVector().normalVector()
+
         p1 = v.p2()
         p2 = n.p2()
         p3 = n2.p2()
+
         pen.setPen(QPen(Qt.darkRed, 2, Qt.SolidLine))
         pen.drawPolygon(p1, p2, p3)
-
-    def drawline(self, tx, ty, mx, my):
-        self.Line_list = [mx, my, tx, ty]
-        self.lines.append([mx, my, tx, ty])
-        self.update()
 
     def drawline_pt(self, s, e):
         self.draw = True
@@ -243,7 +163,6 @@ class Mainwindow(QMainWindow, Ui_MainWindow):
             else:
                 self.lines[s.objectName()].append(e.objectName())
         self.update()
-        self.draw = False
 
     def mousePressEvent(self, event):
         alltag = self.findChildren(QLineEdit)
@@ -261,17 +180,19 @@ class Mainwindow(QMainWindow, Ui_MainWindow):
         wy = self.y()
         x = event.globalX()
         y = event.globalY()
+        
+        self.inittag(x - wx, y - wy)
+
+    def inittag(self, x, y):
         alltag = self.findChildren(QLineEdit)
         print([tag.objectName() for tag in alltag])
-        self.inittag(len(alltag) + 1, x - wx, y - wy)
-        print(2)
-
-    def inittag(self, no, x, y):
-        name = 'tag' + str(no)
+        name = 'tag' + str(len(alltag)+1)
         exec('self.%s = Newlable(self)' % name)
         exec('self.%s.setGeometry(x - 90, y - 90, 180, 60)' % name)
         exec('self.%s.show()' % name)
         exec('self.%s.setObjectName("%s")' % (name, name))
+
+        return eval('self.%s'%name)
 
     def modify_txt(self):
         self.tag1.setTextInteractionFlags(Qt.TextEditorInteraction)
